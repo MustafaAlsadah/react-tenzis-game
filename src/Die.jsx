6224b86abea96de6f2a85789150cbccac5330bd9
MustafaAlsadah <sadah9999@gmail.com>
2023-03-05 15:08:39 +0300

    fix onclick color

--- src/Die.jsx
+++ src/Die.jsx
@@ -3,7 +3,7 @@ export default function Die(props){
     const styles = {backgroundColor: props.isHeld ? '#59E391' : 'white'}
     
     return(
-        <div className="bg-white shadow-md text-black h-20 w-20 rounded justify-center items-center text-center flex items-center font-medium text-3xl hover:scale-110 transition-all"
+        <div className="shadow-md text-black h-20 w-20 rounded justify-center items-center text-center flex items-center font-medium text-3xl hover:scale-110 transition-all"
         style={styles}
         onClick={props.holdDice}>
             {props.value}

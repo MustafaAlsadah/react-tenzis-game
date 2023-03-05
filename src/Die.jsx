d0adca281368b66ac5cb3fd6fdb3ac04446e757c
MustafaAlsadah <sadah9999@gmail.com>
2023-03-05 15:16:19 +0300

    mobile responsive feature

--- src/Die.jsx
+++ src/Die.jsx
@@ -3,7 +3,7 @@ export default function Die(props){
     const styles = {backgroundColor: props.isHeld ? '#59E391' : 'white'}
     
     return(
-        <div className="shadow-md text-black h-20 w-20 rounded justify-center items-center text-center flex items-center font-medium text-3xl hover:scale-110 transition-all"
+        <div className="shadow-md text-black h-10 w-10 text-2xl md:h-20 md:w-20 rounded justify-center items-center text-center flex items-center font-medium md:text-3xl hover:scale-110 transition-all"
         style={styles}
         onClick={props.holdDice}>
             {props.value}
